bc7b73167b174bc0032f4cdac19a67d7499d17e5
Marlon Teixeira <macnuvem@MacProMarlonTeixeira.local>
2023-08-31 10:00:46 -0400

    igual da aula

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando sistema de gerenciamento de jogos"); 
 console.log("Aplicação está se conectando ao banco de dados");
+//mais um console log de teste
+console.log("Alteracoes na aula sem eu ver")
